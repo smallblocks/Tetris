820175bf6cb90ffd5440d641166a980d976d6172
Small Blocks <build@smallblocks.dev>
2026-03-12 17:45:12 +0000

    Fix dependencies.ts to properly declare satscade dependency

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -1,4 +1,10 @@
 import { sdk } from './sdk'
 
 // Depends on SATSCADE arcade launcher
-export const setDependencies = sdk.setupDependencies(async () => ({}))
+export const setDependencies = sdk.setupDependencies(async () => ({
+  satscade: {
+    kind: 'running',
+    versionRange: '*',
+    healthChecks: [],
+  },
+}))
